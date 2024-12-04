4874fcc427a3f733ba272b79bc290d590ecbbb09
Caique Santos Teixeira <c.santos.teixeira@avanade.com>
2024-12-04 18:38:57 -0300

    Alteração dos testes de unidade

--- multiplo.js
+++ multiplo.js
@@ -8,7 +8,8 @@ somaa = multiplos.reduce(function(accumulator, value){
     return accumulator + value
 })
 for (i = 0; i < multiplos.length; i++) {
-    console.log(multiplos[i])
+    //console.log(multiplos[i])
 }
 
+export default somaa
 console.log(`A soma do conjuntos de multiplos entre ${a} e ${b} é: ${somaa}`)
